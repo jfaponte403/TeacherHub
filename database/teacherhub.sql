8f3fa2f239a56f0d5489abd922dad36e01943675
quiles <quilesxasterin8@gmail.com>
2023-10-19 18:51:50 -0500

    [Quiles]: update database

--- database/teacherhub.sql
+++ database/teacherhub.sql
@@ -1,43 +1,62 @@
 CREATE SCHEMA teacherhub;
 
-USE teacherhub;
+SET SCHEMA 'teacherhub';
 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE professor (
-    id_professor BINARY(16) PRIMARY KEY,
+    id_professor BYTEA PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50)
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE subject (
-    id_subject BINARY(16) PRIMARY KEY,
+    id_subject BYTEA PRIMARY KEY,
     name VARCHAR(50)
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE professor_subject (
-    id BINARY(16) PRIMARY KEY,
-    id_professor BINARY(16),
-    id_subject BINARY(16),
+    id BYTEA PRIMARY KEY,
+    id_professor BYTEA,
+    id_subject BYTEA,
     FOREIGN KEY (id_professor) REFERENCES professor (id_professor),
     FOREIGN KEY (id_subject) REFERENCES subject (id_subject)
 );
-CREATE TABLE student (
-    id_student BINARY(16) PRIMARY KEY,
-    first_name VARCHAR(50),
-    email varchar(255),
-    password varchar(255),
-    hash varchar(16)
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  role VARCHAR(50)
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
+CREATE TABLE student (
+    id_student BYTEA PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    hash varchar(16) NOT NULL,
+    is_active boolean,
+    id_role INT,
+    FOREIGN KEY (id_role) REFERENCES role (id)
+);
+
+CREATE TABLE auth_code (
+  id_student BYTEA PRIMARY KEY,
+  date_time timestamp NOT NULL,
+  code varchar(6) NOT NULL,
+  FOREIGN KEY (id_student) REFERENCES student (id_student)
+);
+
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE grade (
-    id BINARY(16) PRIMARY KEY,
-    id_student BINARY(16),
-    id_professor_subject int,
+    id BYTEA PRIMARY KEY,
+    id_student BYTEA,
+    id_professor_subject BYTEA,
     comment text,
     is_positive boolean,
     note int,
     FOREIGN KEY (id_student) REFERENCES student (id_student),
-    FOREIGN KEY (id_professor_subject) REFERENCES professor_subject (id_professor_subject)
+    FOREIGN KEY (id_professor_subject) REFERENCES professor_subject(id)
 );
-
-
