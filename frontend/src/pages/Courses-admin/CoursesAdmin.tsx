c64c95c25b5eea9d11f405f28a24ce5eb3862645
Jhonattan <jfaponte403@gmail.com>
2023-11-06 20:28:20 -0500

    [Jhonattan]: Add CRUD, CoursesAdmin.tsx and UsersAdmin.tsx

--- frontend/src/pages/Courses-admin/CoursesAdmin.tsx
+++ frontend/src/pages/Courses-admin/CoursesAdmin.tsx
@@ -1,46 +1,238 @@
 import NavbarAdmin from "../../components/NavbarAdmin/NavbarAdmin.tsx";
-import { axiosInstance, getData } from "../../api";
-import { useState } from 'react';
+import {axiosInstance, deleteData, getData, postData, putData} from "../../api";
+import {ChangeEvent, useEffect, useState} from 'react';
+import {showAlert} from "../../utils/alertPrompts.ts";
+
+interface Course {
+    id: string;
+    name: string;
+}
 
 const CoursesAdmin = () => {
-    const initialData = [
-        {
-            "id": "bf17389a-69f1-4226-9b38-6dddef3932b1",
-            "name": "Calculo Integral"
-        },
-        {
-            "id": "d2c83095-be06-4821-8348-0c4ea81949fa",
-            "name": "Calculo Multivariado"
-        },
-        {
-            "id": "ec9d9811-9722-4957-8546-f00b80b4cbdd",
-            "name": "Aplicaciones para internet"
-        }
-    ];
+    const [data, setData] = useState<Course[]>([]);
+    const [courseToModify, setCourseToModify] = useState<Course>({
+        id: '',
+        name: ''
+    })
+    const [formToModify, setFormToModify] = useState<boolean>(false)
+    const [formToCreate, setFormToCreate] = useState<boolean>(false)
+    const [nameCourse, setNameCourse] = useState<string>('')
+    const fetchCoursesList = (): void => {
+        getData(axiosInstance, '/teacherhub/api/subjects', {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        })
+            .then(response => {
+                if (response.data) {
+                    setData(response.data);
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
 
-    const [data, setData] = useState(initialData);
+    useEffect(()=>{
+        fetchCoursesList()
+    }, [])
 
-    function test() {
-        console.log(localStorage.getItem('token'));
-        getData(
+    const deleteCourse = (id: string): void => {
+        deleteData(
             axiosInstance,
-            '/teacherhub/api/subjects',
+            `/teacherhub/api/subjects/${id}`,
             {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         ).then(response => {
-            console.log(response);
+            if (response.status === 200) {
+                showAlert(
+                    {
+                        icon: 'success',
+                        title: 'Course has been deleted',
+                        text: 'Thanks for using TeacherHub',
+                        buttonText: 'Ok',
+                    }
+                )
+                fetchCoursesList()
+            }
+        }).catch(error => {
+            console.error(error);
         });
     }
 
-    const handleDelete = (id) => {
-        const updatedData = data.filter(item => item.id !== id);
-        setData(updatedData);
+    const handleInputChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        field: keyof Course
+    ) => {
+        if (courseToModify) {
+            const { value } = event.target;
+            setCourseToModify((prevData: Course) => {
+                if (prevData) {
+                    return {
+                        ...prevData,
+                        [field]: value,
+                    };
+                }
+                return prevData;
+            });
+        }
     };
+
+    const handlerShowForm = (item: Course) => {
+        setFormToModify(true)
+        setCourseToModify(item)
+    }
+
+    const handlerShowFormToCreate = () => {
+        setFormToCreate(true)
+    }
+
+    const handlerExitForm = () => {
+        setFormToModify(false)
+    }
+
+    const handlerExitFormToCreate = () => {
+        setFormToCreate(false)
+    }
+
+    const modifyCourse = () =>{
+        if (courseToModify) {
+            putData(
+                axiosInstance,
+                `/teacherhub/api/subjects`,
+                courseToModify,
+                {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                }
+            )
+                .then((response) => {
+                    if (response.status === 200) {
+                        setFormToModify(false);
+                        showAlert({
+                            icon: "success",
+                            title: "The subject has been modify",
+                            text: "Thanks for using TeacherHub",
+                            buttonText: "Ok",
+                        });
+                        fetchCoursesList()
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }
+
+    const createCourse = () => {
+
+        const newCourse: Course = {
+            id: crypto.randomUUID(),
+            name: nameCourse
+        }
+
+        postData(axiosInstance,
+            '/teacherhub/api/subjects',
+            newCourse,
+            {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            }).then( response => {
+                if (response.status === 200){
+                    setFormToCreate(false)
+                    showAlert({
+                        icon: "success",
+                        title: "The subject has been created",
+                        text: "Thanks for using TeacherHub",
+                        buttonText: "Ok",
+                    });
+                    fetchCoursesList()
+                }
+        }).catch(  error => {
+            console.error(error)
+        })
+    }
 
     return (
         <>
             <NavbarAdmin courses={true} teacher={false} users={false} />
+
+            {
+                formToCreate && (
+                    <div className="modal" tabIndex={-1} role="dialog" style={{ display: "block" }}>
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">CREATE NEW COURSE</h5>
+                                    <button type="button" className="btn btn-outline-orange" onClick={handlerExitFormToCreate}>
+                                        <span>&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Type the new course:</label>
+                                            <input
+                                                type="email"
+                                                className="form-control"
+                                                id="email"
+                                                onChange={(event)=>{setNameCourse(event.target.value)}}
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-outline-orange" onClick={handlerExitFormToCreate}>
+                                        Exit
+                                    </button>
+                                    <button type="button" className="btn btn-orange" onClick={createCourse}>
+                                        Create
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+
+            {
+                formToModify && (
+                    <div className="modal" tabIndex={-1} role="dialog" style={{ display: "block" }}>
+                        <div className="modal-dialog" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title">EDIT COURSE</h5>
+                                    <button type="button" className="btn btn-outline-orange" onClick={handlerExitForm}>
+                                        <span>&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Type de the new course name:</label>
+                                            <input
+                                                type="email"
+                                                className="form-control"
+                                                id="email"
+                                                value={courseToModify.name}
+                                                onChange={(e: ChangeEvent<HTMLInputElement>) => handleInputChange(e, "name")}
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-outline-orange" onClick={handlerExitForm}>
+                                        Exit
+                                    </button>
+                                    <button type="button" className="btn btn-orange" onClick={modifyCourse}>
+                                        Update
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
             <div className="container">
 
                 <div className="text-center mt-4">
@@ -50,9 +242,9 @@ const CoursesAdmin = () => {
                 <table className="table table-striped">
                     <thead>
                     <tr>
-                        <th>Delete</th>
                         <th>Name</th>
                         <th>Modify</th>
+                        <th>Delete</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -62,7 +254,9 @@ const CoursesAdmin = () => {
                             <td>
                                 <button
                                     className="btn btn-primary"
-                                    onClick={() => handleModify(item.id)}
+                                    onClick={()=>{
+                                        handlerShowForm(item)
+                                    }}
                                 >
                                     Modify
                                 </button>
@@ -70,7 +264,7 @@ const CoursesAdmin = () => {
                             <td>
                                 <button
                                     className="btn btn-danger"
-                                    onClick={() => handleDelete(item.id)}
+                                    onClick={()=>{deleteCourse(item.id)}}
                                 >
                                     Delete
                                 </button>
@@ -81,7 +275,7 @@ const CoursesAdmin = () => {
                 </table>
 
                 <div className="text-center mt-4">
-                    <button onClick={test} className="btn btn-outline-orange">Create</button>
+                    <button  className="btn btn-outline-orange" onClick={handlerShowFormToCreate}>Create</button>
                 </div>
 
             </div>
